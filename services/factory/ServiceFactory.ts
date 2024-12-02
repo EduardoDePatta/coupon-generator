2a5697e4d4118cced32aaab0e5f12f97ca1c0fdc
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-12-02 03:39:25 -0300

    feat: create service factory class

---
+++ services/factory/ServiceFactory.ts
@@ -0,0 +1,26 @@
+import { IValidatorUtil, IAuthUtil, ValidatorUtil, AuthUtil } from './../../utils'
+import { GetCouponService, IGetCouponService, IPostCouponService, IRedeemCouponService, PostCouponService, RedeemCouponService } from './../coupon'
+
+class ServiceFactory {
+  private validator: IValidatorUtil
+  private auth: IAuthUtil
+
+  constructor() {
+    this.validator = new ValidatorUtil()
+    this.auth = new AuthUtil()
+  }
+
+  public getCouponService(): IGetCouponService {
+    return new GetCouponService(this.validator, this.auth)
+  }
+
+  public postCouponService(): IPostCouponService {
+    return new PostCouponService(this.validator, this.auth)
+  }
+
+  public redeemCouponService(): IRedeemCouponService {
+    return new RedeemCouponService(this.auth)
+  }
+}
+
+export const serviceFactory = new ServiceFactory()
